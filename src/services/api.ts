8178a227ab99e6ef625cb51f6a461da725aa541a
everaldo@bb-8 <juniorklawa10@gmail.com>
2021-02-12 11:56:41 -0300

    updated public project

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.strackify.com',
-  // baseURL: 'https://strackify-api.herokuapp.com',
-  // baseURL: 'http://10.0.2.2:3333',
+  baseURL: '',
 });
 
 export default api;
